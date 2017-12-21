11d1dd8a6c20a833801d18d418c045dcfb712687
Mark Johnson <Mark.Johnson.62899@gmail.com>
2017-12-21 03:21:08 -0600

    Additions to notes for rpn translation

--- utils/rpn.s
+++ utils/rpn.s
@@ -1,15 +1,26 @@
-.type	rpn, @function
+.type	scc_rpn, @function
 # Prototype. Will be updated next.
-rpn:
-	pushq	8(%rsp) # over
-	call	dup
+scc_rpn:
+	pushq	%rbx
+	movq	%rdi, %rbx
+	pushq	%rbx
+	call	cdr
+	pushq	%rbx
 	call	car
 	leaq	NIL(%rip), %rax
 	pushq	%rax
 	call	cons
-	call	swap
-	call	cdr
-	call	swap
-	call	cons
-	popq	8(%rsp)
+	call	append
+	popq	%rax
+	popq	%rbx
+	ret
+
+.type	rpn, @function
+rpn:
+	movq	8(%rsp), %rdi
+	cmpq	$0, (%rdi)
+	jnz	.rpn_atom
+	call	scc_rpn
+	movq	%rax, 8(%rsp)
+	.rpn_atom:
 	ret
