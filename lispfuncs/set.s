1c515c95972826add7c3b0ac67fcbc7f7caaa46e
Mark Johnson <Mark.Johnson.62899@gmail.com>
2017-12-24 06:01:35 -0600

    Set now checks for nil

--- lispfuncs/set.s
+++ lispfuncs/set.s
@@ -1,7 +1,11 @@
 .type	set, @function
 set: # Stack-based
-	movq	16(%rsp), %rdi # arg 1
-	movq	8(%rsp), %rsi # arg 2
+	movq	16(%rsp), %rdi
+	call	zornil
+	cmpq	$1, %rax
+	je	.set_nil
+	movq	16(%rsp), %rdi
+	movq	8(%rsp), %rsi
 	movq	(%rsi), %rax
 	movq	%rax, (%rdi)
 	movq	8(%rsi), %rax
@@ -9,4 +13,5 @@ set: # Stack-based
 	movq	16(%rsi), %rax
 	movq	%rax, 16(%rdi)
 	popq	(%rsp)
-ret
+	.set_nil:
+	ret
