eae15bbb306f6b105360b2bfbee5e521e133eedd
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-01-05 06:46:08 -0600

    Undid changes to reader memory management

--- lispfuncs/set.s
+++ lispfuncs/set.s
@@ -3,15 +3,29 @@ set: # Stack-based
 	movq	16(%rsp), %rdi
 	call	eqnil
 	cmpq	$1, %rax
-	je	.set_nil
+	je	.set_err
+	pushq	16(%rsp)
+	leaq	T(%rip), %rax
+	pushq	%rax
+	call	eq
+	popq	%rdi
+	call	eqnil
+	cmpq	$1, %rax
+	jne	.set_err
+
 	movq	16(%rsp), %rdi
 	movq	8(%rsp), %rsi
+
 	movq	(%rsi), %rax
 	movq	%rax, (%rdi)
+
 	movq	8(%rsi), %rax
 	movq	%rax, 8(%rdi)
+
 	movq	16(%rsi), %rax
 	movq	%rax, 16(%rdi)
+
 	popq	(%rsp)
-	.set_nil:
+
+	.set_err:
 	ret
