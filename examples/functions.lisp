3d306a4b8f7d9bb8ae9954430a7c4ea5b656f1ef
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-01-18 14:32:57 -0600

    Updated reduce example

--- examples/functions.lisp
+++ examples/functions.lisp
@@ -1,5 +1,6 @@
 (progn
-  (declare 'reduce (lambda '(f l) '((lambda '(f a l) '(cond (l (@ f (f a (car l)) (cdr l))) (t a))) f (f (car l) (car (cdr l))) (cdr (cdr l)))))
+  (define 'reduca (lambda '(f a l) '(cond (l (@ f (f a (car l)) (cdr l))) (t a))))
+  (declare 'reduce (lambda '(f l) '(reduca f (f (car l) (car (cdr l))) (cdr (cdr l)))))
   (declare 'mapcar (lambda '(f l) '(cond ((null l) nil) (t (cons (f (car l)) (@ f (cdr l)))))))
   (declare 'desc (lambda '(o l) '(cond ((null o) l) ((eq (car o) 'a) (@ (cdr o) (car l))) ((eq (car o) 'd) (@ (cdr o) (cdr l))))))
   (declare 'nsplice (lambda '(l) '(reduce nconc l)))
